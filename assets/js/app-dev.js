240bcc23b9e23ed0e610a4120a2cc6aed82ff784
Hammad Ali <ch.hammad2914@gmail.com>
2023-11-15 11:25:56 +0500

    with nav

--- assets/js/app-dev.js
+++ assets/js/app-dev.js
@@ -21,12 +21,34 @@ $('.logos-slider').slick({
     }]
 });
 
+
 // fix form
 function openForm() {
     document.getElementById("quoteForm").style.right = "0";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     document.getElementById("quoteForm").style.right = "-300px";
-  }
-  
+}
+
+// Scroll Btn
+
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+});
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
